a6403b8ab99f125ce7d9e0fd7d6e6e8a9f03650f
Jacob Brandom <brandomjacob@gmail.com>
2016-11-29 18:12:23 -0600

    Binary_Tree.h, BTNode.h, cleanup

--- Project3B/Project3B.cpp
+++ Project3B/Project3B.cpp
@@ -7,11 +7,6 @@ int main()
 	std::string input = "abcdefghijklmnopqrstuvwxyz", input2 = "._ _... _._. _.. . .._. __. .... .. .___ _._ ._.. __ _. ___ .__. __._ ._. ... _ .._ ..._ .__ _.._ _.__ __..";
 	Morse morse;
 	std::cout << morse.encodeMessage(input) << '\n';
-	//std::cout << morse.decoder;
-	//BTNode<std::string>* local_root = morse.decoder.getRoot();
-	//local_root = local_root->left;
-	//std::cout << "left is: " << local_root->data << '\n';
-	//std::cout << local_root->left->data << '\n';
 	std::cout << morse.decodeMessage(input2) << '\n';
 	return 0;
 }
