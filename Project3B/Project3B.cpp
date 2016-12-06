8079a61eb48037499421ba050958d92ab95facd7
Joel Brandom <afro.hj@gmail.com>
2016-12-05 22:16:37 -0600

    Morse decode text update

--- Project3B/Project3B.cpp
+++ Project3B/Project3B.cpp
@@ -7,7 +7,7 @@ int main()
 {
 	// encodeInput will be encoded and outputted in Morse
 	// decodeInput will be decoded and outputted with corresponding alphabetical characters
-	std::string encodeInput = "HelloWorld", decodeInput = "..._ . ._. _.__ __. ___ ___ _.. _.. ___ __.";
+	std::string encodeInput = "HelloWorld", decodeInput = "... _ ._ _.__";
 	Morse morse;
 	morse.populateMapAndTree("morse.txt");
 	std::cout << encodeInput << " encoded: " << morse.encodeMessage(encodeInput) << '\n';
